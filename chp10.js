ffcae487a0f94a52c82c3b3cc61ed051e50dec5a
shahzadausman <shahzadausman48@gmail.com>
2024-09-13 16:15:08 +0500

    Add files via upload

---
+++ chp10.js
@@ -0,0 +1,22 @@
+
+// q........1
+var entercity=prompt("Enter your city name");
+var corretcity="karachi";
+if( entercity==corretcity){
+    alert("welcome to the city of lights");
+}
+else{
+    alert("plz enter you correct city");
+}
+// q.....2
+
+
+var gender=prompt("enter your gender (male/female)")
+var message;
+if(gender=="male"){
+    message="good morning sir";
+}
+else if(gender=="female"){
+    message="good morning ma'am"
+}
+alert(message);
